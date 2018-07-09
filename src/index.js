5375c4083e022ac506bb72a46b211fbbf130ea78
test <demiko2014@gmail.com>
2018-07-10 00:07:59 +0900

    test

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './index.css';
 import renducer from './reducers'
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(renducer, applyMiddleware(thunk));
 
 ReactDOM.render(
     <Provider store={store}>
-        <EventsIndex />
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/events/new" component={ EventsNew }/>
+                <Route exact path="/" component={ EventsIndex }/>
+            </Switch>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 );

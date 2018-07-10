3e5ff3e345f9c47a537d49b4f310034e2ea4eaf7
でみこ <demiko@demiko-no-MacBook-Pro.local>
2018-07-10 16:35:07 +0900

    implemnt event new conmponent

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,19 +1,60 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-//import { postEvent } from '../actions'
+import { Field, reduxForm } from 'redux-form'
+import { postEvent } from '../actions'
 
 class EventsNew extends Component {
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  renderFieled(field) {
+    const { input, label, type, meta: { touched, error } } = field
+    return (
+      <div>
+        <input {...input} placeholder={label} type={type} />
+        {touched && error && <span>{error}</span>}
+      </div>
+    )
+  }
+
+  async onSubmit(values) {
+    console.log('onSubmit')
+    await this.props.postEvent(values)
+    this.props.history.push('/')
+  }
+
   render() {
     console.log("render")
+    const { handleSubmit } = this.props
     return (
-      <React.Fragment>
-        <div>Hi</div>
-      </React.Fragment>
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <div>
+          <Field label="Title" name="title" component={this.renderFieled} />
+          <Field label="Body" name="body" component={this.renderFieled} />
+        </div>
+
+        <div>
+          <input type="submit" valeu="Submit" disabled={false}/>
+          <Link to="/">Cancel</Link>
+        </div>
+      </form>
   )}
 }
 
-const mapStateToProps = store => ({events: store.events})
-const mapDispatchTroProps = ({ })
+const validate = values => {
+  const errors = {}
 
-export default connect(null, mapDispatchTroProps)(EventsNew)
+  if (!values.title) errors.title = "Enter title, please"
+  if (!values.body) errors.body = "Enter body, please"
+  
+  return errors
+}
+
+const mapDispatchTroProps = ({ postEvent })
+
+export default connect(null, mapDispatchTroProps)(
+  reduxForm({ validate, form: "eventNewForm" })(EventsNew)
+)
